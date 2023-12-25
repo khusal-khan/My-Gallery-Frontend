4962bfb917c288754be68407a9cff4d6ffc30a5c
KHUSAL KHAN <95062348+khusal-khan@users.noreply.github.com>
2023-12-26 01:41:48 +0530

    Gallery Frontend

--- src/components/NoImage.jsx
+++ src/components/NoImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const NoImage = () => {
   return (
     <div className='conatiner-fluid text-center'>
-        <img className='no-image' src={`http://localhost:47000/no-image.png`} alt=""/>
+        <img className='no-image' src={`https://my-gallery-backend-production.up.railway.app/no-image.png`} alt=""/>
         <h3>Ready to some Photo?</h3>
         <p>Drag & drop anywhere to upload</p>
     </div>
